d84353a6e1be501fc557125b7819b695fd9dd504
Alisson Almeida de Oliveira <alissonttfire12@gmail.com>
2026-02-06 17:17:27 -0300

    refactor: remover arquivo de configuração do Vercel e ajustar configurações no Vite

--- Kaipe/vite.config.js
+++ Kaipe/vite.config.js
@@ -8,6 +8,18 @@ export default defineConfig({
     outDir: 'dist', // Define o diretório de saída como 'dist'
     emptyOutDir: true, // Limpa o diretório antes de build
     assetsDir: 'assets', // Subdiretório para assets
+    rollupOptions: {
+      output: {
+        manualChunks: void 0,
+      }
+    }
   },
   base: '/', // Base path corrigido para aplicações SPA
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  }
 })
