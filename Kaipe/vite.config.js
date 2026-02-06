b70e62c47b7795026709e66851f39f4d0a0e81df
Alisson Almeida de Oliveira <alissonttfire12@gmail.com>
2026-02-06 17:03:25 -0300

    fix: corrigir diretório de saída no arquivo de configuração do Vercel e ajustar configurações de build no Vite

--- Kaipe/vite.config.js
+++ Kaipe/vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Define o diretório de saída como 'dist'
+    emptyOutDir: true, // Limpa o diretório antes de build
+    assetsDir: 'assets', // Subdiretório para assets
   },
+  base: './', // Base path para os assets
 })
